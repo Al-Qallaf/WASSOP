c9b29b4b303356b513101a1886a0523f3a87165a
Ali Hameed Al-Qallaf <ahhahalqalaf@dundee.ac.uk>
2011-11-18 16:38:06 +0000

    Second sprint: final delivered story

--- src/wasso/dundee/ac/uk/viewtotal.java
+++ src/wasso/dundee/ac/uk/viewtotal.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 public class viewtotal extends Activity{
 
-    //Integer test = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) throws SQLException{
         // TODO Auto-generated method stub
@@ -31,16 +30,14 @@ public class viewtotal extends Activity{
         String[][] data = new String[6][4];
         data=info.getDataFromDB();
         info.close();
-        //tv.setText(data[0][0] + " " + data[0][1] + " " + data[0][2]);
-        //==========================1=====================================
-        //==========================1=====================================
+
         TextView row1 = (TextView) findViewById(R.id.row11);
         row1.setText(data[0][1]);
         TextView row11 = (TextView) findViewById(R.id.row12);
         String fnumber1 = calcTotalWorth(data[0][2], data[0][3]);
         row11.setText(fnumber1.toString());
-        Float worth11 = Float.parseFloat(fnumber1); 
-        //row11.setText(data[0][3]);
+        Integer worth11 = Integer.parseInt(fnumber1); 
+
 		if (data[0][3].equals("00") || data[0][3].equals("000") || data[0][3].equals("0000") ) 
 		{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR1);
@@ -63,16 +60,13 @@ public class viewtotal extends Activity{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR1);
 			tr1.setBackgroundColor(Color.parseColor("#FFA500"));
 		}
-		
-        //===========================2====================================
-        //===========================2====================================        
-        TextView row2 = (TextView) findViewById(R.id.row21);
+
+		TextView row2 = (TextView) findViewById(R.id.row21);
         row2.setText(data[1][1]);
         TextView row22 = (TextView) findViewById(R.id.row22);
         String fnumber2 = calcTotalWorth(data[1][2], data[1][3]);
         row22.setText(fnumber2.toString());
-        Float worth22 = Float.parseFloat(fnumber2);
-        //row22.setText(data[1][3]);
+        Integer worth22 = Integer.parseInt(fnumber2);
         if (data[1][3].equals("00") || data[1][3].equals("000") || data[1][3].equals("0000") ) 
 		{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR2);
@@ -96,16 +90,13 @@ public class viewtotal extends Activity{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR2);
 			tr1.setBackgroundColor(Color.parseColor("#FFA500"));
 		}
-		
-        //============================3===================================
-        //============================3===================================       
+		       
         TextView row3 = (TextView) findViewById(R.id.row31);
         row3.setText(data[2][1]);
         TextView row33 = (TextView) findViewById(R.id.row32);
         String fnumber3 = calcTotalWorth(data[2][2], data[2][3]);
         row33.setText(fnumber3.toString());
-        Float worth33 = Float.parseFloat(fnumber3);
-        //row33.setText(data[2][3]);
+        Integer worth33 = Integer.parseInt(fnumber3);
         if (data[2][3].equals("00") || data[2][3].equals("000") || data[2][3].equals("0000") ) 
 		{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR3);
@@ -129,15 +120,14 @@ public class viewtotal extends Activity{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR3);
 			tr1.setBackgroundColor(Color.parseColor("#FFA500"));
 		}
-        //=============================4==================================
-        //=============================4==================================        
+ 
         TextView row4 = (TextView) findViewById(R.id.row41);
         row4.setText(data[3][1]);
         TextView row44 = (TextView) findViewById(R.id.row42);
         String fnumber4 = calcTotalWorth(data[3][2], data[3][3]);
         row44.setText(fnumber4.toString());
-        Float worth44 = Float.parseFloat(fnumber4);
-        //row44.setText(data[3][3]);
+        Integer worth44 = Integer.parseInt(fnumber4);
+
         if (data[3][3].equals("00") || data[3][3].equals("000") || data[3][3].equals("0000") ) 
 		{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR4);
@@ -161,15 +151,13 @@ public class viewtotal extends Activity{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR4);
 			tr1.setBackgroundColor(Color.parseColor("#FFA500"));
 		}
-        //==============================5=================================
-        //==============================5=================================
+
         TextView row5 = (TextView) findViewById(R.id.row51);
         row5.setText(data[4][1]);
         TextView row55 = (TextView) findViewById(R.id.row52);
         String fnumber5 = calcTotalWorth(data[4][2], data[4][3]);
         row55.setText(fnumber5.toString());
-        Float worth55 = Float.parseFloat(fnumber5);
-        //row55.setText(data[4][3]);
+        Integer worth55 = Integer.parseInt(fnumber5);
         if (data[4][3].equals("00") || data[4][3].equals("000") || data[4][3].equals("0000") ) 
 		{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR5);
@@ -193,15 +181,14 @@ public class viewtotal extends Activity{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR5);
 			tr1.setBackgroundColor(Color.parseColor("#FFA500"));
 		}
-        //==============================5=================================
-        //==============================5=================================
+
         TextView row6 = (TextView) findViewById(R.id.row61);
         row6.setText(data[5][1]);
         TextView row66 = (TextView) findViewById(R.id.row62);
         String fnumber6 = calcTotalWorth(data[5][2], data[5][3]);
         row66.setText(fnumber6.toString());
-        Float worth66 = Float.parseFloat(fnumber6);
-        //row55.setText(data[4][3]);
+        Integer worth66 = Integer.parseInt(fnumber6);
+
         if (data[5][3].equals("00") || data[5][3].equals("000") || data[5][3].equals("0000") ) 
 		{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR6);
@@ -225,11 +212,8 @@ public class viewtotal extends Activity{
 			TableRow tr1 = (TableRow) findViewById(R.id.TR6);
 			tr1.setBackgroundColor(Color.parseColor("#FFA500"));
 		}
-        //===============================================================
-        //===============================================================
 
-      //=========================The total======================================
-        Float totalWorthValue = (worth11+worth22+worth33+worth44+worth55+worth66);
+		Integer totalWorthValue = (worth11+worth22+worth33+worth44+worth55+worth66);
         TextView totalRow = (TextView) findViewById(R.id.row72);
         totalRow.setText(totalWorthValue.toString());
         }
@@ -239,7 +223,6 @@ public class viewtotal extends Activity{
             Dialog TheD = new Dialog(this);
             TextView tv = new TextView(this);
             tv.setText(e.getMessage());
-            //tv.setText("Here");
             TheD.setContentView(tv);
             TheD.show();
         }
@@ -254,7 +237,7 @@ public class viewtotal extends Activity{
     	}
     	else
     	{
-    		Float res = (Float.parseFloat(unit) * Float.parseFloat(price));
+    		Integer res = (Integer.parseInt(unit) * Integer.parseInt(price))/100;
     	    return res.toString();
     	}
     }
